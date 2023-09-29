8f10f544f99e01c34978834a12f95625ba15e706
oimasterkafuu <oimasterfake@icloud.com>
2023-09-29 10:29:58 +0800

    test(test/chatgpt): merge valid array test together
    Merge the tests which check the array is valid and length greater than 0 together

--- test/chatgpt.test.js
+++ test/chatgpt.test.js
@@ -12,11 +12,9 @@ describe('Test fetch steps', function () {
 
     it('should return an array of steps', function () {
         expect(chat).to.be.an('array');
-    });
-    it('should return an array of steps with length > 0', function () {
         expect(chat.length).to.be.greaterThan(0);
     });
-    it(`should return an array that every item is made of {"time": "Time required in minutes", "detail": "Single step task required to complete"}`, function () {
+    it(`should return an array in valid format`, function () {
         chat.forEach((step) => {
             expect(step).to.have.all.keys('time', 'detail');
         });
