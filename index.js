dcbe8433f636414b6951d389511e5331a9070805
oimasterkafuu <oimasterfake@icloud.com>
2023-09-29 16:06:13 +0800

    refactor(index): print url to console
    Print http://localhost:${port} to console to make it easier for developers to open the browser.

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const port = config.port;
 const app = require('./app');
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
