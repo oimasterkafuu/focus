4007d6c3a24323c1d2b9fc3fa0f6913fc9006b5c
oimasterkafuu <oimasterfake@icloud.com>
2023-09-29 09:42:07 +0800

    refactor(libs/chatgpt): modify function name
    fetchChatGPT() do not provide any prompt now
    
    BREAKING CHANGE: fetchChatGPT() has changed

--- libs/chatgpt.js
+++ libs/chatgpt.js
@@ -7,16 +7,13 @@ myHeaders.append('Authorization', `Bearer ${key}`);
 myHeaders.append('User-Agent', 'Mozilla/5.0');
 myHeaders.append('Content-Type', 'application/json');
 
-async function fetchChatGPT(text) {
+async function fetchChatGPT(content) {
     const raw = JSON.stringify({
         model: 'gpt-3.5-turbo',
         messages: [
             {
                 role: 'user',
-                content: `You are the assistant of the FOCUS app. The user will provide you his/her tasks. You should answer in a json array where each item is in the format {"time": "Time required in minutes", "detail": "Single step task required to complete"}.
-For example, if the user says {{Write an article about flowers}}
-You should answer [{"time":"20","detail":"Research flowers"},{"time":"10","detail":"Outline article"},{"time":"10","detail":"Write introduction"},{"time":"30","detail":"Write body"},{"time":"20","detail":"Write conclusion"},{"time":"20","detail":"Proofread article"}] (a valid JSON of several steps, not containing any other character)
-Now user says {{${text}}}`
+                content
             }
         ]
     });
@@ -33,9 +30,7 @@ Now user says {{${text}}}`
         let text = await response.text();
         let result = JSON.parse(text);
         let message = result.choices[0].message.content;
-        console.log(message);
         let parsed = JSON.parse(message);
-        console.log(parsed);
         return parsed;
     } catch (error) {
         console.log('error', error);
@@ -43,6 +38,16 @@ Now user says {{${text}}}`
     }
 }
 
+async function fetchSteps(content) {
+    let result =
+        await fetchChatGPT(`You are the assistant of the FOCUS app. The user will provide you his/her tasks. You should answer in a json array where each item is in the format {"time": "Time required in minutes", "detail": "Single step task required to complete"}.
+Use the user's language to provide the details.
+For example, if the user says {{Write an article about flowers}}
+You should answer [{"time":"20","detail":"Research flowers"},{"time":"10","detail":"Outline article"},{"time":"10","detail":"Write introduction"},{"time":"30","detail":"Write body"},{"time":"20","detail":"Write conclusion"},{"time":"20","detail":"Proofread article"}] (a valid JSON of several steps, not containing any other character)
+Now user says {{${content}}}`);
+    return result;
+}
+
 module.exports = {
-    fetchChatGPT
+    fetchSteps
 };
--- test/chatgpt.test.js
+++ test/chatgpt.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { fetchChatGPT } = require('../libs/chatgpt');
+const { fetchSteps } = require('../libs/chatgpt');
 
 describe('Test fetch steps', function () {
     this.timeout(10000);
@@ -7,17 +7,17 @@ describe('Test fetch steps', function () {
     let chat;
 
     before(async function () {
-        chat = await fetchChatGPT('Finish my two-page math homework in 30 minutes');
+        chat = await fetchSteps('Finish my two-page math homework in 30 minutes');
     });
 
-    it('should return an array of steps', async function () {
-        await expect(chat).to.be.an('array');
+    it('should return an array of steps', function () {
+        expect(chat).to.be.an('array');
     });
-    it('should return an array of steps with length > 0', async function () {
-        await expect(chat.length).to.be.greaterThan(0);
+    it('should return an array of steps with length > 0', function () {
+        expect(chat.length).to.be.greaterThan(0);
     });
-    it(`should return an array that every item is made of {"time": "Time required in minutes", "detail": "Single step task required to complete"}`, async function () {
-        await chat.forEach((step) => {
+    it(`should return an array that every item is made of {"time": "Time required in minutes", "detail": "Single step task required to complete"}`, function () {
+        chat.forEach((step) => {
             expect(step).to.have.all.keys('time', 'detail');
         });
     });
