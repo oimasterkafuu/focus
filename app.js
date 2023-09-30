f77179289ded37ae5cf8806c662f33e1ae59c39f
oimasterkafuu <oimasterfake@icloud.com>
2023-09-30 19:22:03 +0800

    perf(views): remove ejs
    Since the app is not a multi-page app, remove ejs module.
    
    BREAKING CHANGE: EJS module is removed now.

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ const { apiRouter } = require('./routers/api');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-app.get('/alpha-version', (req, res) => {
-    res.render('alpha');
-});
-
 app.use('/api/', apiRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
