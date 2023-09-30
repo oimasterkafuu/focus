12de4d6fcae29fe5a2aa29c644354bc377dc9280
oimasterkafuu <oimasterfake@icloud.com>
2023-09-30 15:01:39 +0800

    feat(routers/api): add fetchHint to express API
    Allow client to post /api/fetchHints to get hints.

--- routers/api.js
+++ routers/api.js
@@ -1,11 +1,19 @@
 const { Router } = require('express');
-const { fetchSteps } = require('../libs/chatgpt');
+const { fetchSteps, fetchHints } = require('../libs/chatgpt');
 
 const router = Router();
 router.post('/fetchSteps/', async (req, res) => {
     let result = await fetchSteps(req.body.task);
     if (result) {
         res.send({ success: true, steps: result });
+        return;
+    }
+    res.send({ success: false });
+});
+router.post('/fetchHints/', async (req, res) => {
+    let result = await fetchHints(req.body.task);
+    if (result) {
+        res.send({ success: true, hints: result });
         return;
     }
     res.send({ success: false });
--- test/client.test.js
+++ test/client.test.js
@@ -19,3 +19,18 @@ describe('POST /api/fetchSteps', function () {
         expect(response.body.steps.length).to.be.greaterThan(0);
     });
 });
+
+describe('POST /api/fetchHints', function () {
+    this.timeout(10000);
+    let response;
+    before(async () => {
+        response = await request(app).post('/api/fetchHints/').type('form').send({ task: 'Finish my homework' });
+    });
+    it('should return hints', () => {
+        expect(response.status).to.be.equal(200);
+        expect(response.body).to.be.an('object');
+        expect(response.body.success).to.be.equal(true);
+        expect(response.body.hints).to.be.an('array');
+        expect(response.body.hints.length).to.be.at.least(5);
+    });
+});
